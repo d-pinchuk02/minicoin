bbe01cec33679babcd3e310343a2260f0da45ac3
Dmitriy Pinchuk <d.pinchuk02@gmail.com>
2021-07-31 16:32:41 +0300

    Redesign history page table

--- src/components/HistoryTable.vue
+++ src/components/HistoryTable.vue
@@ -1,38 +1,61 @@
 <template>
-  <v-data-table
-    hide-default-footer
-    item-key="id"
-    :headers="headers"
-    :items="items"
-    :items-per-page="15"
-    :page.sync="page"
-    @page-count="$parent.pageCountChanged($event)"
-  >
-    <template v-slot:item.amount="{ item }">
-      {{ item.amount | currency }}
-    </template>
-
-    <template v-slot:item.date="{ item }">
-      {{ item.date | date('datetime') }}
-    </template>
-
-    <template v-slot:item.type="{ item }">
-      <v-chip :color="item.typeClass" text-color="white">
-        {{ item.typeText }}
-      </v-chip>
-    </template>
-
-    <template v-slot:item.open="{ item }">
-      <v-btn
-        color="primary"
-        :to="`/detail/${item.id}/page/${page}`"
-      >
-        <v-icon>
-          mdi-open-in-new
-        </v-icon>
-      </v-btn>
-    </template>
-  </v-data-table>
+  <div>
+    <v-data-table
+      v-model="selected"
+      hide-default-footer
+      show-select
+      item-key="id"
+      :headers="headers"
+      :items="items"
+      :items-per-page="15"
+      :page.sync="page"
+      @page-count="$parent.pageCountChanged($event)"
+    >
+      <template v-slot:item.amount="{ item }">
+        {{ item.amount | currency }}
+      </template>
+    
+ 	 <template v-slot:item.type="{ item }">
+        <v-chip :color="item.typeClass" text-color="white">
+          {{ item.typeText }}
+        </v-chip>
+      </template>
+    
+      <template v-slot:item.date="{ item }">
+        {{ item.date | date('datetime') }}
+      </template>
+    
+      <template v-slot:item.actions="{ item }">
+        <v-btn
+          icon
+          color="primary"
+          :to="`/detail/${item.id}/page/${page}`"
+          :title="localizeFilter('historytable.openRecord')"
+        >
+          <v-icon>
+            mdi-open-in-new
+          </v-icon>
+        </v-btn>
+        <!--<v-btn
+          icon
+          :title="localizeFilter('historytable.editRecord')"
+        >
+          <v-icon>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
+        <v-btn
+          icon
+          color="red"
+          :title="localizeFilter('historytable.deleteRecord')"
+        >
+          <v-icon>
+            mdi-delete
+          </v-icon>
+        </v-btn>-->
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -48,9 +71,10 @@ export default {
     records: {
       required: true,
       type: Array
-    }
+    },
   },
   data: () => ({
+    selected: [],
     headers: [
       {
         text: '#',
@@ -58,10 +82,10 @@ export default {
         value: 'index'
       },
       { text: localizeFilter('shared.amount'), value: 'amount' },
-      { text: localizeFilter('shared.date'), value: 'date' },
-      { text: localizeFilter('shared.category'), value: 'categoryName' },
       { text: localizeFilter('shared.type'), value: 'type' },
-      { text: localizeFilter('historytable.open'), value: 'open' },
+      { text: localizeFilter('shared.category'), value: 'categoryName' },	
+      { text: localizeFilter('shared.date'), value: 'date' },
+      { text: localizeFilter('historytable.actions'), value: 'actions' },
     ],
     pageCount: 1
   }),
